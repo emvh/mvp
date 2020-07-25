ecfc5ed04f9d54ccc93f75f811f8a2ce834a5ec5
emvh <emilyvho@gmail.com>
2020-07-25 10:43:04 -0700

    Setup basic folder structure

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  document.getElementById('app'),
+);
