837aae06d6d2c06e09095030ace9b17b593a2b4e
emvh <emilyvho@gmail.com>
2020-07-26 19:29:37 -0700

    Get event data from database

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Events from './Events.jsx';
 
 class App extends React.Component {
@@ -6,7 +7,32 @@ class App extends React.Component {
     super();
     this.state = {
       view: 'events',
+      events: [],
+      isLoaded: false,
     };
+    this.renderView = this.renderView.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios({
+      method: 'get',
+      url: '/api/events',
+    })
+      .then((response) => {
+        const events = response.data;
+        this.setState({
+          events,
+          isLoaded: true,
+        });
+      })
+      .catch((error) => {
+        this.setState({ isLoaded: true });
+        console.log(error);
+      });
   }
 
   renderView() {
@@ -14,37 +40,49 @@ class App extends React.Component {
     if (view === 'home') {
       return (
         <div>
-
-          <span><button>Create an Event</button></span>
-          <span><button>Find an Event</button></span>
-
+          <span>
+            <button>Create an Event</button>
+          </span>
+          <span>
+            <button>Find an Event</button>
+          </span>
         </div>
-      )
+      );
     }
     if (view === 'adoption') {
-      console.log('adoption feed');
-      return <div>adoption</div>
+      return (<div>adoption</div>);
     }
     if (view === 'events') {
-      console.log('events');
-      return <div>
-        eventsview
-        <Events />
+      const { events } = this.state;
+      return (
+        <div>
+          <Events
+            events={events}
+          />
         </div>
+      );
     }
   }
 
   render() {
+    const { isLoaded } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
 
-        <div className='nav'>
-          <span className='section'
-            onClick={() => this.changeView('feed')}>
+        <div className="nav">
+          <span
+            className="section"
+            onClick={ () => this.changeView('feed') }
+          >
             Find Love
           </span>
           <span
-            onClick={() => this.changeView('events')}>
+            onClick={ () => this.changeView('events') }
+          >
             Find Pawties
           </span>
         </div>
